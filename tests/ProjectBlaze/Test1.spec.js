18a1be11790a99daad76415d9f7d9ebecda1b596
PeaceGusenga <gusengapeace@gmail.com>
2023-05-01 13:44:52 +0200

    demoblaze view cart

--- tests/ProjectBlaze/Test1.spec.js
+++ tests/ProjectBlaze/Test1.spec.js
@@ -1,9 +1,21 @@
 //add specific modules required
-const {test, expect} = require('@playwright/test')
+const { test, expect } = require('@playwright/test');
 
-test ('Visit Site', async ({page}) => {
-    //visit DemoBlaze
-    await page.goto('https://www.demoblaze.com/index.html')
-    //Check Site Title
-    await expect(page).toHaveTitle('demoblaze')
-})
+// async keyword waits for the page to load and run steps sequentailly which is necesary 
+//for testing
+test('visit site', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/blaze/);
+  });
+  
+  test('view cart', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com');
+  
+    // Click the get started link.
+    await page.getByRole('link',{name:'Cart'}).click();
+  
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*cart/);
+  });
